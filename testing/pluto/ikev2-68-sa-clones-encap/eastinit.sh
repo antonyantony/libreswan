6e28cb031bb58ce03f42f8f76e83ca37023306ba
Antony Antony <antony@phenome.org>
2019-03-22 23:42:35 +0100

    testing: ikev2 clone test cases wih clones=n

---
+++ testing/pluto/ikev2-68-sa-clones-encap/eastinit.sh
@@ -0,0 +1,7 @@
+/testing/guestbin/swan-prep
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet
+ipsec auto --status | grep westnet-eastnet
+ipsec whack --impair suppress-retransmits
+echo "initdone"
