19edf390af07c4276f342042eef6fc1832d90135
Antony Antony <antony@phenome.org>
2019-03-22 21:58:25 +0000

    testing

--- testing/pluto/ikev2-67-parallel-sa/northrun.sh
+++ testing/pluto/ikev2-67-parallel-sa/northrun.sh
@@ -1,8 +1,11 @@
 ipsec auto --status | grep northnet-eastnets
 ipsec auto --up northnet-eastnet
-taskset 0x1 ping -n -c 2 -I 192.0.3.254 192.0.2.254
+taskset 0x1 ping -n -c 2 -I 192.0.3.254 192.0.2.254 
 taskset 0x2 ping -n -c 2 -I 192.0.3.254 192.0.2.254
-numactl --physcpubind=0 ping -n -c 2 -I 192.0.3.254 192.0.2.254
-numactl --physcpubind=1 ping -n -c 2 -I 192.0.3.254 192.0.2.254
+#numactl --physcpubind=0 ping -n -f -I 192.0.3.254 192.0.2.254 &
+#numactl --physcpubind=1 ping -n -f -I 192.0.3.254 192.0.2.254 &
+#numactl --physcpubind=2 ping -n -f -I 192.0.3.254 192.0.2.254 &
+#numactl --physcpubind=3 ping -n -f -I 192.0.3.254 192.0.2.254 &
+#numactl --physcpubind=4 ping -n -f -I 192.0.3.254 192.0.2.254 &
 ipsec whack --trafficstatus
 echo done
