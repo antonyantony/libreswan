51da2421a6a41b2c73cf84fa855890d8101d798f
Antony Antony <antony@phenome.org>
2019-05-01 05:30:48 +0200

    ikev2: outgoing sub SA perturb UDP src port for RSS entropy on RX side
    RSS does not seems to distribute ESP well, even though one halfo of spi
    is aligned with UDP SRC port. One way to overcome this is
    ESP encap in UDP, with randomized src port.
    
    Though ENCAP in UDP is performance hit, this activate RSS.
    Kernel UDP GRO may need improvements to get better performance.
    
    Also set the SRC port on the recevier side, soe xfrm won't generate
    mapping change message.

--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -1250,6 +1250,10 @@ static bool extract_connection(struct fd *whackfd,
 		loglog(RC_FATAL, "MOBIKE requires ikev2");
 		return false;
 	}
+	if (wm->sa_clones != 0 && c->ike_version == IKEv1) {
+		loglog(RC_FATAL, "clones= requires ikev2");
+		return false;
+	}
 	if (wm->policy & POLICY_IKEV2_ALLOW_NARROWING &&
 	    c->ike_version == IKEv1) {
 		loglog(RC_FATAL, "narrowing=yes requires ikev2");
@@ -1824,7 +1828,7 @@ static bool extract_connection(struct fd *whackfd,
 			DBG_log("AA_2020 %s %d %s use head's reqid %u", __func__, __LINE__, c->name, c->spd.reqid);
 			if  (c->spd.reqid == 0) {
 				libreswan_log("AA_2020 can not find head reqid");
-				return;
+				return false;
 			}
 		}
 	} else {
