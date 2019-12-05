2f2e89de24bcabfb2860678a57d061d1dd71d9ac
Antony Antony <antony@phenome.org>
2019-12-05 14:23:23 +0000

    lib: parser add binary prefix support
    needed for samaxbytes and samaxpackets
    
     - Binary prefixes Ki,Mi,Gi,Ti,Pi,Ei ISO/IEC standard packets
     - Bytes prefixes B,KiB,MiB,GiB,TiB,PiB,EiB ISO/IEC standard
    
    https://en.wikipedia.org/wiki/Binary_prefix

--- lib/libipsecconf/confread.c
+++ lib/libipsecconf/confread.c
@@ -352,6 +352,8 @@ static bool load_setup(struct starter_config *cfg,
 		case kt_enum:
 		case kt_number:
 		case kt_time:
+		case kt_binary:
+		case kt_byte:
 		case kt_percent:
 			/* all treated as a number for now */
 			assert(f < elemsof(cfg->setup.options));
@@ -891,6 +893,8 @@ static bool translate_field(struct starter_conn *conn,
 	case kt_enum:
 	case kt_number:
 	case kt_time:
+	case kt_binary:
+	case kt_byte:
 	case kt_percent:
 		/* all treated as a number for now */
 		assert(opt_floor <= field && field < opt_roof);
@@ -999,6 +1003,7 @@ static bool translate_conn(struct starter_conn *conn,
 						/*set_options*/&conn->options_set,
 						/*opt_floor*/KNCF_last_leftright + 1,
 						/*opt_roof*/KNCF_ROOF);
+
 		}
 	}
 	return serious_err;
