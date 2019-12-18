5915af2b3dd0a9f0c21e4bb2758fb28f32865757
Antony Antony <antony@phenome.org>
2019-12-18 06:37:11 +0000

    connections: add desired_state auto= vaule to connection
    previously the auto=ondemand was not distinguishable from
    ipsec auto --up.
    Now connection, c->desired_state, records the initial auto=value.
    
    this is necessary for clones work.

--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -1551,6 +1551,7 @@ static bool extract_connection(struct fd *whackfd,
 			};
 		}
 
+		c->desired_state = wm->desired_state;
 		c->nic_offload = wm->nic_offload;
 		c->sa_ike_life_seconds = wm->sa_ike_life_seconds;
 		c->sa_ipsec_life_seconds = wm->sa_ipsec_life_seconds;
