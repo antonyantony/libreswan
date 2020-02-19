e0cc26bf44711515f6949667c178a8068f509d03
Antony Antony <antony@phenome.org>
2020-02-19 10:49:27 +0000

    pluto: move struct keyword_auto from ipsecconf/keywords.h to pluto_constants.h
    otherwise need to add ipsecconf/keywords.h in connections.h
    which would lead conflicting definations of struct keyword
    So this is a compromise for now
    
    /home/build/libreswan/include/keywords.h:61:8: error: redefinition of 'struct keyword'
       61 | struct keyword {
          |        ^~~~~~~
    /home/build/libreswan/include/ipsecconf/keywords.h:377:8: note: originally defined here
      377 | struct keyword {
          |        ^~~~~~~

--- include/ipsecconf/keywords.h
+++ include/ipsecconf/keywords.h
@@ -308,15 +308,6 @@ enum keyword_keyexchange {
 	KE_IKE  = 1,
 };
 
-/* values for auto={add,start,route,ignore} */
-enum keyword_auto {
-	STARTUP_IGNORE     = 0,
-	STARTUP_POLICY     = 1,
-	STARTUP_ADD        = 2,
-	STARTUP_ONDEMAND   = 3,
-	STARTUP_START      = 4
-};
-
 /*
  * Potential keyword values for fields like {left,right}rsasigkey=.
  *
