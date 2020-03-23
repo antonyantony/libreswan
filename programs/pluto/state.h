c6f23c39c715d2b2e252c29469982b655fe3f555
Antony Antony <antony@phenome.org>
2020-03-23 18:13:37 +0000

    ikev2: queue up IKEv2 informational, delete, message instead of jamming
    Pluto, when initiating a new IKEv2 informational, an IKEv2
    Delete child message(v2D), initiator did not check un-acknowledged messages.
    It would just jam and would violate IKEv2 window of one. The result is
    rekey response would be dropped, and message window would jump.
    
    This is a partial fix, add v2D message to the tail of the queue.
    where IKEv2 CREATE_CHILD_SA(rekey) are queued.

--- programs/pluto/state.h
+++ programs/pluto/state.h
@@ -825,6 +825,7 @@ extern void discard_state(struct state **st);
 extern void delete_states_by_connection(struct connection *c, bool relations);
 extern void rekey_p2states_by_connection(struct connection *c);
 extern void delete_my_family(struct state *pst, bool v2_responder_state);
+extern void schedule_next_child_delete(struct state *st, struct ike_sa *ike);
 
 struct state *ikev1_duplicate_state(struct state *st, struct fd *whackfd);
 struct child_sa *ikev2_duplicate_state(struct ike_sa *st, enum sa_type sa_type,
