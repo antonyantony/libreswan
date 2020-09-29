antonyantony/libreswan

commit 9f3ef2f22b8326ef732910e4372167d0228b45f8

IKEv1: Support for XFRMi interfaces

programs/pluto/ikev1_main.c
@@ -92,6 +92,10 @@
 #include "nss_cert_verify.h"
 #include "iface.h"
 
+#ifdef USE_XFRM_INTERFACE
+# include "kernel_xfrm_interface.h"
+#endif
+
 /*
  * Initiate an Oakley Main Mode exchange.
  * --> HDR;SA
@@ -1576,7 +1580,7 @@ stf_status main_inI3_outR3(struct state *st, struct msg_digest *md)
 		if (r != STF_OK)
 			return r;
 	}
-	const struct connection *c = st->st_connection;
+	struct connection *c = st->st_connection;
 
 	/* send certificate if we have one and auth is RSA */
 	cert_t mycert = c->spd.this.cert;
@@ -1766,6 +1770,11 @@ stf_status main_inI3_outR3(struct state *st, struct msg_digest *md)
 	}
 
 	ISAKMP_SA_established(st);
+#ifdef USE_XFRM_INTERFACE
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 	linux_audit_conn(st, LAK_PARENT_START);
 	return STF_OK;
 }
@@ -1788,6 +1797,7 @@ stf_status main_inR3(struct state *st, struct msg_digest *md)
 		if (r != STF_OK)
 			return r;
 	}
+	struct connection *c = st->st_connection;
 
 	/* Done input */
 
@@ -1795,15 +1805,15 @@ stf_status main_inR3(struct state *st, struct msg_digest *md)
 	 * It seems as per Cisco implementation, XAUTH and MODECFG
 	 * are not supposed to be performed again during rekey
 	 */
-	if (st->st_connection->remotepeertype == CISCO &&
-		st->st_connection->newest_isakmp_sa != SOS_NOBODY &&
-		st->st_connection->spd.this.xauth_client) {
+	if (c->remotepeertype == CISCO &&
+		c->newest_isakmp_sa != SOS_NOBODY &&
+		c->spd.this.xauth_client) {
 		DBG(DBG_CONTROL,
 			DBG_log("Skipping XAUTH for rekey for Cisco Peer compatibility."));
 		st->hidden_variables.st_xauth_client_done = TRUE;
 		st->st_oakley.doing_xauth = FALSE;
 
-		if (st->st_connection->spd.this.modecfg_client) {
+		if (c->spd.this.modecfg_client) {
 			DBG(DBG_CONTROL,
 				DBG_log("Skipping ModeCFG for rekey for Cisco Peer compatibility."));
 			st->hidden_variables.st_modecfg_vars_set = TRUE;
@@ -1812,6 +1822,11 @@ stf_status main_inR3(struct state *st, struct msg_digest *md)
 	}
 
 	ISAKMP_SA_established(st);
+#ifdef USE_XFRM_INTERFACE
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 	linux_audit_conn(st, LAK_PARENT_START);
 
 	passert((st->st_policy & POLICY_PFS) == 0 ||

programs/pluto/ikev1_aggr.c
@@ -47,6 +47,10 @@
 #include "pending.h"
 #include "iface.h"
 
+#ifdef USE_XFRM_INTERFACE
+# include "kernel_xfrm_interface.h"
+#endif
+
 /* STATE_AGGR_R0: HDR, SA, KE, Ni, IDii
  *           --> HDR, SA, KE, Nr, IDir, HASH_R/SIG_R
  */
@@ -838,6 +842,11 @@ static stf_status aggr_inR1_outI2_tail(struct msg_digest *md)
 	DBG(DBG_CONTROL, DBG_log("phase 1 complete"));
 
 	ISAKMP_SA_established(st);
+#ifdef USE_XFRM_INTERFACE
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 
 	linux_audit_conn(st, LAK_PARENT_START);
 	return STF_OK;
@@ -959,6 +968,11 @@ stf_status aggr_inI2(struct state *st, struct msg_digest *md)
 	DBG(DBG_CONTROL, DBG_log("phase 1 complete"));
 
 	ISAKMP_SA_established(st);
+#ifdef USE_XFRM_INTERFACE
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 
 	linux_audit_conn(st, LAK_PARENT_START);
 	return STF_OK;

programs/pluto/ikev1_quick.c
@@ -84,6 +84,10 @@
 
 #include <blapit.h>
 
+#ifdef USE_XFRM_INTERFACE
+# include "kernel_xfrm_interface.h"
+#endif
+
 const struct dh_desc *ikev1_quick_pfs(const struct child_proposals proposals)
 {
 	if (proposals.p == NULL) {
@@ -1382,6 +1386,7 @@ static stf_status quick_inI1_outR1_continue12_tail(struct msg_digest *md,
 						   struct pluto_crypto_req *r)
 {
 	struct state *st = md->st;
+	struct connection *c = st->st_connection;
 	struct payload_digest *const id_pd = md->chain[ISAKMP_NEXT_ID];
 	struct payload_digest *const sapd = md->chain[ISAKMP_NEXT_SA];
 
@@ -1537,6 +1542,11 @@ static stf_status quick_inI1_outR1_continue12_tail(struct msg_digest *md,
 	 * We do this before any state updating so that
 	 * failure won't look like success.
 	 */
+#ifdef USE_XFRM_INTERFACE
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 	if (!install_inbound_ipsec_sa(st))
 		return STF_INTERNAL_ERROR; /* ??? we may be partly committed */
 
@@ -1762,6 +1772,11 @@ stf_status quick_inR1_outI2_tail(struct msg_digest *md,
 	 * We do this before any state updating so that
 	 * failure won't look like success.
 	 */
+#ifdef USE_XFRM_INTERFACE
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 	if (!install_ipsec_sa(st, TRUE))
 		return STF_INTERNAL_ERROR;
 
@@ -1795,6 +1810,12 @@ stf_status quick_inI2(struct state *st, struct msg_digest *md UNUSED)
 	 * We do this before any state updating so that
 	 * failure won't look like success.
 	 */
+#ifdef USE_XFRM_INTERFACE
+	struct connection *c = st->st_connection;
+	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+		if (add_xfrmi(c))
+			return STF_FATAL;
+#endif
 	if (!install_ipsec_sa(st, FALSE))
 		return STF_INTERNAL_ERROR;
 

programs/pluto/ikev2_parent.c
@@ -3411,6 +3411,7 @@ static stf_status ikev2_process_ts_and_rest(struct msg_digest *md)
 {
 	struct child_sa *child = pexpect_child_sa(md->st);
 	struct state *st = &child->sa;
+	struct connection *c = st->st_connection;
 
 	RETURN_STF_FAILURE_STATUS(ikev2_process_cp_respnse(md));
 	if (!v2_process_ts_response(child, md)) {
@@ -3433,7 +3434,7 @@ static stf_status ikev2_process_ts_and_rest(struct msg_digest *md)
 
 	/* check for Child SA related NOTIFY payloads */
 	if (md->v2N.use_transport_mode) {
-		if (st->st_connection->policy & POLICY_TUNNEL) {
+		if (c->policy & POLICY_TUNNEL) {
 			/* This means we did not send v2N_USE_TRANSPORT, however responder is sending it in now, seems incorrect */
 			dbg("Initiator policy is tunnel, responder sends v2N_USE_TRANSPORT_MODE notification in inR2, ignoring it");
 		} else {
@@ -3453,7 +3454,7 @@ static stf_status ikev2_process_ts_and_rest(struct msg_digest *md)
 		struct ikev2_notify_ipcomp_data n_ipcomp;
 
 		dbg("received v2N_IPCOMP_SUPPORTED of length %zd", len);
-		if ((st->st_connection->policy & POLICY_COMPRESS) == LEMPTY) {
+		if ((c->policy & POLICY_COMPRESS) == LEMPTY) {
 			loglog(RC_LOG_SERIOUS, "Unexpected IPCOMP request as our connection policy did not indicate support for it");
 			return STF_FAIL + v2N_NO_PROPOSAL_CHOSEN;
 		}
@@ -3487,9 +3488,9 @@ static stf_status ikev2_process_ts_and_rest(struct msg_digest *md)
 #ifdef USE_XFRM_INTERFACE
 	/* before calling do_command() */
 	if (st->st_state->kind != STATE_V2_REKEY_CHILD_I)
-		if (st->st_connection->xfrmi != NULL &&
-				st->st_connection->xfrmi->if_id != yn_no)
-			if (add_xfrmi(st->st_connection))
+		if (c->xfrmi != NULL &&
+				c->xfrmi->if_id != yn_no)
+			if (add_xfrmi(c))
 				return STF_FATAL;
 #endif
 	/* now install child SAs */

commit 701e3ce0a7c7e2be139c1ca5af039635e1220797

pluto: ikev1 xfrmi can not install xfrmi only when IKE is established
This was too early to instll xfrmi. It should be after
IPsec SA are installed.

Fixes: 9f3ef2f22b83 ("IKEv1: Support for XFRMi interfaces")

programs/pluto/ikev1_main.c
@@ -1713,11 +1713,6 @@ stf_status main_inI3_outR3(struct state *st, struct msg_digest *md)
 	}
 
 	IKE_SA_established(pexpect_ike_sa(st));
-#ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
-		if (add_xfrmi(c, st->st_logger))
-			return STF_FATAL;
-#endif
 	linux_audit_conn(st, LAK_PARENT_START);
 	return STF_OK;
 }
@@ -1768,11 +1763,6 @@ stf_status main_inR3(struct state *st, struct msg_digest *md)
 	}
 
 	IKE_SA_established(pexpect_ike_sa(st));
-#ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
-		if (add_xfrmi(c, st->st_logger))
-			return STF_FATAL;
-#endif
 	linux_audit_conn(st, LAK_PARENT_START);
 
 	passert((st->st_policy & POLICY_PFS) == 0 ||