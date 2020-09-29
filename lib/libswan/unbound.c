antonyantony/libreswan

commit 2158b617be4382d7e07a72103bc6d3038681e4b5

ttoaddr, tnatoaddr, ttosubnet can discover the address family based on the string If the af parameter is 0, each routine will accept either v4 or v6. This feature existed and was used, but was not documented. - added documentation to ipsec_ttoaddr(3) - replaced 0 with AF_UNSPEC to make this more evident. - modest tidying - virtual-private now uses this feature if %v4: and %v6: are missing

lib/libswan/unbound.c
@@ -111,7 +111,7 @@ bool unbound_init(struct ub_ctx *dnsctx)
 /*
  * synchronous blocking resolving - simple replacement of ttoaddr()
  * src_len == 0 means "apply strlen"
- * af == 0 means "try both families
+ * af == AF_UNSPEC means "try both families"
  */
 bool unbound_resolve(struct ub_ctx *dnsctx, char *src, size_t srclen, int af,
 		ip_address *ipaddr)
@@ -196,7 +196,7 @@ bool unbound_resolve(struct ub_ctx *dnsctx, char *src, size_t srclen, int af,
 		err_t err = tnatoaddr(
 			inet_ntop(af, result->data[0], dst,
 				(af == AF_INET) ? INET_ADDRSTRLEN :
-				INET6_ADDRSTRLEN),
+					INET6_ADDRSTRLEN),
 			0, af, ipaddr);
 		ub_resolve_free(result);
 		if (err == NULL) {

lib/libswan/ttoaddr.3.xml
@@ -239,9 +239,12 @@ and
 <function>ttosubnet</function>
 specifies the address family of interest.
 It should be either
-<emphasis remap='B'>AF_INET</emphasis>
+<emphasis remap='B'>AF_INET</emphasis>,
+<emphasis remap='B'>AF_INET6</emphasis>,
 or
-<emphasis remap='B'>AF_INET6</emphasis>.</para>
+<emphasis remap='B'>AF_UNSPEC</emphasis>.
+If it is <emphasis remap='B'>AF_UNSPEC</emphasis>,
+the family will be based on the string.</para>
 
 <para>The
 <varname role='parameter'>dstlen</varname>

lib/libswan/ttoaddr.c
@@ -55,7 +55,7 @@ ttoaddr_base(const char *src,
 	switch (af) {
 	case AF_INET:
 	case AF_INET6:
-	case 0:	/* guess */
+	case AF_UNSPEC:	/* guess */
 		break;
 
 	default:
@@ -71,7 +71,7 @@ ttoaddr_base(const char *src,
 	}
 
 	if (memchr(src, ':', srclen) != NULL) {
-		if (af == 0)
+		if (af == AF_UNSPEC)
 			af = AF_INET6;
 
 		if (af != AF_INET6)
@@ -80,7 +80,7 @@ ttoaddr_base(const char *src,
 		return colon(src, srclen, dst);
 	}
 
-	if (af == 0 || af == AF_INET) {
+	if (af == AF_UNSPEC || af == AF_INET) {
 		oops = trydotted(src, srclen, dst);
 		if (oops == NULL)
 			return NULL;	/* it worked */
@@ -112,7 +112,7 @@ ip_address *dst;
 	}
 
 	switch (af) {
-	case 0:	/* guess */
+	case AF_UNSPEC:	/* guess */
 		oops = colon(src, srclen, dst);
 		if (oops == NULL)
 			return NULL;
@@ -465,12 +465,11 @@ ttoaddr(const char *src,
 		nultermd = 1;
 	} else {
 		nultermd = 0;	/* at least, not *known* to be terminated */
-
 	}
 	err = ttoaddr_base(src, srclen, af, &numfailed, dst);
 
 	if (err && numfailed) {
-		if (af == 0) {
+		if (af == AF_UNSPEC) {
 			err = tryname(src, srclen, nultermd, AF_INET6, dst);
 			if (err)
 				err = tryname(src, srclen, nultermd, AF_INET,
@@ -568,7 +567,7 @@ void regress()
 			oops = ttoaddr_num(in, strlen(in), 0, &a);
 		} else {
 			/* convert it *to* internal format */
-			oops = ttoaddr(in, strlen(in), 0, &a);
+			oops = ttoaddr(in, strlen(in), AF_UNSPEC, &a);
 		}
 
 		if (r->expectfailure && oops == NULL) {

lib/libswan/ttorange.c
@@ -61,7 +61,7 @@ ip_range *dst;
 	if (oops != NULL)
 		return oops;
 
-	if (af == 0)
+	if (af == AF_UNSPEC)
 		af = ip_address_family(&addr_start_tmp);
 
 	switch (af) {
@@ -69,11 +69,8 @@ ip_range *dst;
 		break;
 	case AF_INET6:
 		return "address family (AF_INET6) is not supported in ttorange start";
-
 	default:
 		return "unknown address family in ttorange start";
-
-		break;
 	}
 
 	/*extract end ip address*/
@@ -81,19 +78,13 @@ ip_range *dst;
 	if (oops != NULL)
 		return oops;
 
-	if (af == 0)
-		af = ip_address_family(&addr_end_tmp);
-
 	switch (af) {
 	case AF_INET:
 		break;
 	case AF_INET6:
 		return "address family (AF_INET6) is not supported in ttiporange end";
-
 	default:
 		return "unknown address family in ttorange end";
-
-		break;
 	}
 	if (ntohl(addr_end_tmp.u.v4.sin_addr.s_addr) <
 		ntohl(addr_start_tmp.u.v4.sin_addr.s_addr))

lib/libswan/ttosubnet.c
@@ -36,32 +36,40 @@ ip_subnet *dst;
 	size_t mlen;
 	const char *oops;
 	unsigned long bc;
-	static char def[] = DEFAULTSUBNET;
+	static const char def[] = DEFAULTSUBNET;
 #define DEFLEN (sizeof(def) - 1)	/* -1 for NUL */
-	static char defis4[] = "0/0";
+	static const char defis4[] = "0/0";
 #define DEFIS4LEN (sizeof(defis4) - 1)
-	static char defis6[] = "::/0";
+	static const char defis6[] = "::/0";
 #define DEFIS6LEN (sizeof(defis6) - 1)
 	ip_address addrtmp;
 	ip_address masktmp;
 	int nbits;
 	int i;
 
-	if (srclen == 0)
+	if (srclen == 0) {
 		srclen = strlen(src);
-	if (srclen == 0)
-		return "empty string";
+		if (srclen == 0)
+			return "empty string";
+	}
 
 	/*
-	 * you cannot use af==0 and src=0/0,
+	 * you cannot use af==AF_UNSPEC and src=0/0,
 	 * makes no sense as will it be AF_INET
 	 */
-	if (af == 0 && srclen == DEFLEN && strncmp(src, def, srclen) == 0)
-		return "unknown address family with 0/0 subnet not allowed.";
-
 	if (srclen == DEFLEN && strncmp(src, def, srclen) == 0) {
-		src = (af == AF_INET) ? defis4 : defis6;
-		srclen = (af == AF_INET) ? DEFIS4LEN : DEFIS6LEN;
+		switch (af) {
+		case AF_INET:
+			src = defis4;
+			srclen = DEFIS4LEN;
+			break;
+		case AF_INET6:
+			src = defis6;
+			srclen = DEFIS6LEN;
+			break;
+		default:
+			return "unknown address family with " DEFAULTSUBNET " subnet not allowed.";
+		}
 	}
 
 	slash = memchr(src, '/', srclen);
@@ -74,7 +82,7 @@ ip_subnet *dst;
 	if (oops != NULL)
 		return oops;
 
-	if (af == 0)
+	if (af == AF_UNSPEC)
 		af = ip_address_family(&addrtmp);
 
 	switch (af) {
@@ -86,8 +94,6 @@ ip_subnet *dst;
 		break;
 	default:
 		return "unknown address family in ttosubnet";
-
-		break;
 	}
 
 	/* extract port, as last : */

linux/net/ipsec/addrtot.c
@@ -407,7 +407,7 @@ void regress()
 		strcpy(in, r->input);
 
 		/* convert it *to* internal format */
-		oops = ttoaddr(in, strlen(in), 0, &a);
+		oops = ttoaddr(in, strlen(in), AF_UNSPEC, &a);
 
 		/* now convert it back */
 

linux/net/ipsec/anyaddr.c
@@ -41,17 +41,11 @@ ip_address *dst;
 	case AF_INET:
 		return initaddr((unsigned char *)&v4any, sizeof(v4any), af,
 				dst);
-
-		break;
 	case AF_INET6:
 		return initaddr((unsigned char *)&v6any, sizeof(v6any), af,
 				dst);
-
-		break;
 	default:
 		return "unknown address family in anyaddr/unspecaddr";
-
-		break;
 	}
 }
 
@@ -80,17 +74,11 @@ ip_address *dst;
 	case AF_INET:
 		return initaddr((unsigned char *)&v4loop, sizeof(v4loop), af,
 				dst);
-
-		break;
 	case AF_INET6:
 		return initaddr((unsigned char *)&v6loop, sizeof(v6loop), af,
 				dst);
-
-		break;
 	default:
 		return "unknown address family in loopbackaddr";
-
-		break;
 	}
 }
 
@@ -118,8 +106,6 @@ const ip_address * src;
 
 	default:
 		return 0;
-
-		break;
 	}
 
 	return (cmp == 0) ? 1 : 0;
@@ -153,8 +139,6 @@ const ip_address * src;
 		break;
 	default:
 		return 0;
-
-		break;
 	}
 
 	return (cmp == 0) ? 1 : 0;

linux/net/ipsec/initaddr.c
@@ -30,8 +30,6 @@ unsigned short port;
 		break;
 	default:
 		return "unknown address family in add_port";
-
-		break;
 	}
 	return NULL;
 }
@@ -87,8 +85,6 @@ ip_address *dst;
 		break;
 	default:
 		return "unknown address family in initaddr";
-
-		break;
 	}
 	return NULL;
 }

commit 7168aa65c973f472df21dfb9e6b158d82b4fa3d2

unbound: remove a missleading comment never supported AF_UNSPEC
The comment is mislead unsupported feature. The comment have perpatued
through several changes.  There is no evidence of actual support of AF_UNSPEC.

Commit histories
Fixes: 2158b617be43 ("ttoaddr, tnatoaddr, ttosubnet can discover")
Fixes: 7868b07e1f1a ("Use bool where appropriate in unbound.c")
Fixes: 38a689528e3b ("* Had forgotten to add lib/liblibreswan/unboud.c")

lib/libswan/unbound.c
@@ -230,7 +230,7 @@ void unbound_sync_init(bool do_dnssec, const char *rootfile,
 /*
  * synchronous blocking resolving - simple replacement of ttoaddr()
  * src_len == 0 means "apply strlen"
- * af == AF_UNSPEC means "try both families"
+ * af == AF_UNSPEC means default to AF_INET(A/IPv4)
  */
 bool unbound_resolve(char *src, size_t srclen, int af, ip_address *ipaddr, struct logger *logger)
 {